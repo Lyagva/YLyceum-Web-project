98598462ef3b9200369cc7b3bf1dd9f2f292ec68
Lyagva <githublyagva@gmail.com>
2022-03-22 19:42:45 +0300

    Изменён Readme

--- Server.py
+++ Server.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
 import Logger
 
 log = Logger.set_logging() #  Задаём логгер. Для логов надо писать log.<уровень>(<текст>)
 
 app = Flask(__name__)
 app.config["SECRET_CODE"] = "Very strange thing"
-socketio = SocketIO(app) #  Задаём сервер с системой socket
 
 
 
 if __name__ == "__main__":
     log.info("Server Starting")
-    socketio.run(app) #  Запускаем сокет-сервер
+    app.run("", 5000)
