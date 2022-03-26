e8358510a972b48fb1767c7f5dde968175b3d7a3
Lyagva <githublyagva@gmail.com>
2022-03-26 15:34:35 +0300

    ip login sys - finished

--- Command/commands.py
+++ Command/commands.py
@@ -15,13 +15,20 @@ def command_help(addr, *args):
 def command_debug(addr, *args):
     from datetime import datetime
     return_data = f"sender address \t {addr}\n"
-    return_data += f"server time \t {datetime.now()}"
+    return_data += f"server time \t {datetime.now()}\n"
+    return_data += f"user \t {find_user_by_ip(addr) if find_user_by_ip(addr) else 'user not found'}"
 
     return return_data
 
 
 def get_users():
     return list(map(lambda user: user, db_session.create_session().query(__all_models.Users).all()))
+
+def find_user_by_ip(addr):
+    for user in get_users():
+        if user.ip == addr:
+            return user.login
+    return None
 
 
 def command_login(addr, *args):
@@ -41,9 +48,18 @@ def command_login(addr, *args):
 
         if not user.check_password(password):
             return 'The name is busy or the password is incorrect'
+        else:
+            db_sess = db_session.create_session()
 
-        elif user.check_password(password):
+            prev_user = find_user_by_ip(addr)
+            if prev_user:
+                prev_user = db_sess.query(__all_models.Users).get(prev_user)
+                prev_user.ip = None
+
+            user = db_sess.query(__all_models.Users).get(user.login)
             user.ip = addr
+            db_sess.commit()
+
             return 'You successfully logged to your account'
 
     user = __all_models.Users()
@@ -51,6 +67,13 @@ def command_login(addr, *args):
     user.set_password(password)
     user.ip = addr
     db_sess = db_session.create_session()
+
+    prev_user = find_user_by_ip(addr)
+    if prev_user:
+        prev_user = db_sess.query(__all_models.Users).get(prev_user)
+        prev_user.ip = None
+
     db_sess.add(user)
     db_sess.commit()
+
     return 'You successfully created new account'
