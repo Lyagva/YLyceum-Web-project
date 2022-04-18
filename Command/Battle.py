946c88a5ea4ee6196050dad575968f3d91a0ae31
Lyagva <githublyagva@gmail.com>
2022-04-18 21:32:13 +0300

    All Battle system! \(￣︶￣*\))

--- Command/Battle.py
+++ Command/Battle.py
@@ -7,7 +7,13 @@ import Command.commands
 
 log = logging.getLogger()
 base_session = {"members": []}
-battle_sessions = {-228: {"members": ["name1", "name2"]}}
+
+# {<Battle ID>: {"members": ["name1", "name2"], "type": "pvp"/"pve", "turn": 0}}
+battle_sessions = {}
+
+# {<Battle ID>: {"dmg": 30, "energy": 5, "energy_cost": 2, "hp": 100, "def": 10}}
+bots = {}
+
 
 
 def get_battle_id(username):
@@ -32,10 +38,11 @@ def get_battle_stats(key, username):
     """
     Функция выводит боевые статы [username]'а по ключу [key]
     Ключи:
-        melee - урон в ближнем бою
-        range - урон от огнестрела
+        melee - статы оружия ближнего боя
+        range - статы оружия дальнего боя
         eng - сколько осталось энергии
         ha - heal amount
+        def - defence
 
     :param key: ключ
     :param username: имя пользователя
@@ -46,6 +53,11 @@ def get_battle_stats(key, username):
         return get_melee(username)
     if key == "range":
         return get_range(username)
+    if key == "def":
+        return get_defence(username)
+    if key == "eng":
+        return get_energy(username)
+    return -1
 
 def get_heal_amount(username):
     db_sess = db_session.create_session()
@@ -91,7 +103,7 @@ def get_melee(username):
     output = {}
     for item in weapons:
         if Command.commands.localize(item.name, username) == weapon_name:
-            output = {"dmg": item.damage, "ecost": item.energyCost, "prc": item.piercing}
+            output = {"dmg": item.damage, "eng": item.energyCost, "prc": item.piercing, "ch": 100}
             break
 
     db_sess.commit()
@@ -115,7 +127,7 @@ def get_range(username):
     output = -1
     for item in weapons:
         if Command.commands.localize(item.name, username) == weapon_name:
-            output = {"dmg": item.damage, "ecost": item.energyCost, "prc": item.piercing, "hch": item.hitChance}
+            output = {"dmg": item.damage, "eng": item.energyCost, "prc": item.piercing, "ch": item.hitChance}
             break
 
     db_sess.commit()
@@ -124,9 +136,82 @@ def get_range(username):
         return {}
     return output
 
+def get_defence(username):
+    defence = 0
+    defence += Command.commands.get_user_params(username)["defence"]
+
+    user_params = db_session.create_session().query(__all_models.UsersParams).get(username)
+    armors = db_session.create_session().query(__all_models.ItemArmor)
+    armor = json.loads(user_params.equipment)["ItemArmor"]
+    armor_defence = []
+
+    for item in armors:
+        if Command.commands.localize(item.name, username) in armor.values():
+            armor_defence.append(item.defence)
+
+    defence += sum(armor_defence) ** 0.5
+
+    return defence
+
+def get_energy(username):
+    user_params = db_session.create_session().query(__all_models.UsersParams).get(username)
+    return json.loads(user_params.stats)["energy"]
+
+def turn_check(username):
+    if battle_sessions[get_battle_id(username)]["members"] \
+        [battle_sessions[get_battle_id(username)]["turn"]] == username \
+            and (len(battle_sessions[get_battle_id(username)]["members"]) >= 2 or
+                 battle_sessions[get_battle_id(username)]["type"] == "pve"):
+        return True
+    return False
+
+def send_message(username, text):
+    from app import console_outputs
+
+    console_outputs[Command.commands.encode_name(username)].append(text)
+
+
+def create(username, *args):
+    if len(args) < 1:
+        return "No lobby type provided"
+
+    lobby_type = args[0]
+
+    battle_id = random.randint(0, 1000)
+    while battle_id in battle_sessions.keys():
+        battle_id = random.randint(0, 1000)
+
+    battle_sessions[battle_id] = {"members": [], "type": "pvp" if lobby_type == "pvp" else "pve"}
+    enter(username, battle_id)
+
+    if battle_sessions[battle_id]["type"] == "pve":
+        bots[battle_id] = {"dmg": get_battle_stats("melee", username)["dmg"] * 1.2,
+                           "def": get_battle_stats("def", username) * 1.2,
+                           "energy": 5,
+                           "energy_cost": 2,
+                           "hp": 100}
+        bots[battle_id]["money"] = bots[battle_id]["dmg"] * bots[battle_id]["def"]
+
+    return f"Created lobby {battle_id} with {battle_sessions[battle_id]['type']} type. You already in."
+
 
 def turn(battle_id):
-    battle_sessions[battle_id]["members"] = battle_sessions[battle_id]["members"][::-1]
+    battle_sessions[battle_id]["turn"] = (battle_sessions[battle_id]["turn"] + 1) % 2
+
+    if battle_sessions[battle_id]["type"] == "pve" and battle_sessions[battle_id]["turn"] == 1:
+        pve_turn(battle_id)
+
+
+def pve_turn(battle_id):
+    bot = bots[battle_id]
+
+    if bot["energy"] < bot["energy_cost"]:
+        return_msg = bot_pass(battle_id)
+    else:
+        return_msg = bot_attack(battle_id)
+
+    send_message(battle_sessions[battle_id]["members"][0], return_msg)
+    turn(battle_id)
 
 
 def enter(username, *args):
@@ -142,10 +227,10 @@ def enter(username, *args):
         battle_sessions[battle_id] = base_session
     battle_sessions[battle_id]["members"].append(username)
 
-    return "You entered battle " + str(battle_id)
+    return f"You entered battle {battle_id}"
 
 
-def leave(username, *args):
+def leave(username):
     battle_id = get_battle_id(username)
     battle_sessions[battle_id]["members"].remove(username)
     if len(battle_sessions[battle_id]) <= 0:
@@ -155,9 +240,14 @@ def leave(username, *args):
 
 
 def escape(username):
+    if not turn_check(username):
+        return "It's not your turn!"
+
     chance = random.random() * 100
 
     if chance > 50:
+        send_message(get_enemy(username, get_battle_id(username)), "Your enemy escaped from battle")
+
         leave(username)
 
         return "You successfully escaped battle"
@@ -165,6 +255,65 @@ def escape(username):
 
 
 def attack(username, *args):
+    if not turn_check(username):
+        return "It's not your turn!"
+
+    battle_id = get_battle_id(username)
+    turn(battle_id)
+    lobby_type = battle_sessions[battle_id]["type"]
+
+
+    if lobby_type == "pvp":
+        enemy = get_enemy(username, battle_id)
+        if len(args) < 1:
+            send_message(enemy, "Your enemy skipped turn")
+            return "No attack type provided. Use 'melee' or 'range'"
+        attack_type = args[0]
+
+        if attack_type == "melee":
+            send_message(enemy, "Your enemy skipped turn")
+            weapon = get_battle_stats("melee", username)
+        elif attack_type == "range":
+            send_message(enemy, "Your enemy skipped turn")
+            weapon = get_battle_stats("range", username)
+        else:
+            send_message(enemy, "Your enemy skipped turn")
+            return "Invalid attack type"
+        if weapon == {}:
+            send_message(enemy, "Your enemy skipped turn")
+            return "No weapon"
+
+        if get_battle_stats("eng", username) < weapon["eng"]:
+            send_message(enemy, "Your enemy skipped turn")
+            return "You have less energy than needed. Needed: " + str(weapon["eng"] - get_battle_stats("eng", username))
+
+        Command.commands.edit_user_stats("energy", -weapon["eng"], "+", username)
+
+        dmg = weapon["dmg"] ** 0.5 + Command.commands.get_user_params(username)["attack"]
+
+        if random.randint(0, 100) <= round(dmg * weapon["prc"] * weapon["ch"] / get_battle_stats("def", enemy) / 100):
+            dmg /= get_battle_stats("def", enemy)
+            Command.commands.edit_user_stats("hp", -dmg, "+", enemy)
+
+            send_message(enemy, f"Your enemy attacked you by {dmg} HP")
+
+            result = death_check(battle_id)
+            if result:
+                Command.commands.edit_user_stats("money",
+                                                 Command.commands.get_user_params(
+                                                     enemy)["money"] / 4,
+                                                 "+", username)
+                Command.commands.edit_user_stats("money",
+                                                 -Command.commands.get_user_params(
+                                                     enemy)["money"] / 4,
+                                                 "+", enemy)
+                leave(username)
+                leave(enemy)
+
+            return "Successfully attacked enemy by " + str(dmg) + "HP"
+        send_message(enemy, "Your enemy skipped turn")
+        return "You didn't hit enemy"
+
     if len(args) < 1:
         return "No attack type provided. Use 'melee' or 'range'"
     attack_type = args[0]
@@ -178,22 +327,41 @@ def attack(username, *args):
     if weapon == {}:
         return "No weapon"
 
-    dmg = weapon["dmg"]
+    if get_battle_stats("eng", username) < weapon["eng"]:
+        return "You have less energy than needed. Needed: " + str(weapon["eng"] - get_battle_stats("eng", username))
 
-    enemy = get_enemy(username, get_battle_id(username))
-    Command.commands.edit_user_stats("hp", -dmg, "+", enemy)
+    Command.commands.edit_user_stats("energy", -weapon["eng"], "+", username)
 
-    return "Successfully attacked enemy by " + str(dmg) + "HP"
+    dmg = weapon["dmg"] ** 0.5 + Command.commands.get_user_params(username)["attack"]
 
+    if random.randint(0, 100) <= \
+            round(dmg * weapon["prc"] * weapon["ch"] / bots[battle_id]["def"] / 100):
 
-def block(username, *args):
-    pass
+        dmg /= bots[battle_id]["def"]
+        bots[battle_id]["hp"] -= dmg
+
+        result = death_check(battle_id)
+
+        if result:
+            Command.commands.edit_user_stats("money",
+                                             bots[battle_id]["money"],
+                                             "+", username)
+
+            leave(username)
+
+        return "Successfully attacked enemy by " + str(dmg) + "HP"
+    return "You didn't hit enemy"
 
 
 def heal(username, *args):
+    if not turn_check(username):
+        return "It's not your turn!"
+    turn(get_battle_id(username))
+
     heal_amount = get_battle_stats("ha", username)
 
     if heal_amount == -1:
+        send_message(get_enemy(username, get_battle_id(username)), "Your enemy skipped turn")
         return "Error while healing"
 
     Command.commands.edit_user_stats("hp", heal_amount, "+", username)
@@ -202,11 +370,59 @@ def heal(username, *args):
         heal_amount = 100 - heal_amount
         Command.commands.edit_user_stats("hp", 100, "=", username)
 
+    send_message(get_enemy(username, get_battle_id(username)), f"Your enemy successfully healed by {heal_amount} HP")
+
     return "You successfully healed by " + str(heal_amount) + \
            "HP. Your HP - " + str(Command.commands.get_user_params(username)["hp"])
 
 
 def battle_pass(username, *args):
+    if not turn_check(username):
+        return "It's not your turn!"
+    turn(get_battle_id(username))
+
     energy_regenerated = 5
     Command.commands.edit_user_stats("energy", energy_regenerated, "+", username)
+    send_message(get_enemy(username, get_battle_id(username)), "Your enemy skipped turn")
     return f"Use successfully regenerated {energy_regenerated} energy"
+
+
+def bot_attack(battle_id):
+    bot = bots[battle_id]
+
+    dmg = bot["dmg"]
+
+    Command.commands.edit_user_stats("hp", -dmg, "+", battle_sessions[battle_id]["members"][0])
+
+    return f"Enemy bot hit you by {dmg} HP"
+
+
+def bot_pass(battle_id):
+    bot = bots[battle_id]
+
+    bot["energy"] += 5
+
+    return "Enemy bot regenerated 5 energy"
+
+
+def death_check(battle_id):
+    lobby_type = battle_sessions[battle_id]["type"]
+
+    if lobby_type == "pve":
+        if bots[battle_id]["health"] <= 0:
+            send_message(battle_sessions[battle_id]["members"][0],  "You won!")
+            return True
+
+        player = battle_sessions[battle_id]["members"][0]
+        if get_battle_stats("hp", player) <= 0:
+            send_message(player, "You lost...")
+            return True
+    else:
+        players = battle_sessions[battle_id]["members"]
+
+        for player in players:
+            if get_battle_stats("hp", player) <= 0:
+                send_message(player, "You lost...")
+                send_message(players.copy().remove(player)[0], "You won!")
+                return True
+    return False
